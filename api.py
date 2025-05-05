d0d9275782d6eedc3e99ef50028830cf110d37da
Leonel-M <leonel.amarquez92@gmail.com>
2025-05-05 16:36:03 -0500

    refactor: Separates request logic and saving in functions

--- api.py
+++ api.py
@@ -1,5 +1,4 @@
 #  Code base of https://fastapi.tiangolo.com/tutorial/first-steps/
-
 from fastapi import FastAPI
 import json
 import httpx
@@ -8,25 +7,41 @@ The HTTPX library is required to connect correctly and asynchronously with exter
 https://stackoverflow.com/questions/63872924/how-can-i-send-an-http-request-from-my-fastapi-app-to-another-site-api?utm_source=chatgpt.com
 """
 
-app = FastAPI()
+async def fetch_data(client, url):
+    """
+    :param client:
+    :param url: Platzi API
+    :return: Server data in JSON format
+    """
+    response = await client.get(url)
+    return response.json()  # https://fakeapi.platzi.com/ has e-commerce data in JSON format
 
+def save_json(data,filename):
+    """
+    :param data: Server data
+    :param filename: Document location
+    :return:
+    """
+    # https://www.geeksforgeeks.org/reading-and-writing-json-to-a-file-in-python/
+    with open(filename, 'w', encoding='utf-8') as outfile:
+        json.dump(data, outfile, ensure_ascii=False, indent=4 )
+
+app = FastAPI()
 
 @app.get("/")
 async def get_data():
     api_products = 'https://api.escuelajs.co/api/v1/products'
-    async with httpx.AsyncClient() as client:  # Async requests https://www.python-httpx.org/async/
-        products_response = await client.get(api_products)
-        products = products_response.json()  # https://fakeapi.platzi.com/ has e-commerce data in JSON format
-
     api_locations = 'https://api.escuelajs.co/api/v1/locations'
-    async with httpx.AsyncClient() as client:
-        locations_response = await client.get(api_locations)
-        locations = locations_response.json()
 
-# https://www.geeksforgeeks.org/reading-and-writing-json-to-a-file-in-python/
-    with open('data/products.json', 'w', encoding='utf-8') as outfile:
-        json.dump(products, outfile, ensure_ascii=False, indent=4 )
-    with open('data/locations.json', 'w', encoding='utf-8') as outfile:
-        json.dump(locations, outfile, ensure_ascii=False, indent=4 )
+    async with httpx.AsyncClient() as client:  # Async requests https://www.python-httpx.org/async/
 
-    return
+        products = await fetch_data(client,api_products)
+        locations = await fetch_data(client,api_locations)
+
+        save_json(products, 'data/products.json')
+        save_json(locations, 'data/locations.json')
+    return {
+        'message': 'Data saved successfully',
+        'products_count': len(products),
+        'locations_count': len(locations)
+    }

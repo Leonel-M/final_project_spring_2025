43030d39f2fa00b77ae0d50e3275472d72be2bd5
Leonel-M <leonel.amarquez92@gmail.com>
2025-05-06 09:16:25 -0500

    refactor: Adds file existence verification and JSONDecodeError handling in get_data

--- data.py
+++ data.py
@@ -1,13 +1,21 @@
 import pandas as pd
 import json
+import os
 
 def get_data(path):
     """
     :param path: Json file Path
     :return: Pandas DataFrames
     """
-    with open(path,'r', encoding='utf-8') as file:
-        json_data = json.load(file)
+    # Using os.path to Check if a File Exists https://blog.openreplay.com/python-check-if-file-exists/
+    if not os.path.isfile(path):
+        raise FileNotFoundError(f'{path} does not exist.')
+    else:
+        try:
+            with open(path,'r', encoding='utf-8') as file:
+                json_data = json.load(file)
+        except json.JSONDecodeError as e:
+            raise ValueError(f'JSON invalid on {path}')
         # https://pandas.pydata.org/docs/reference/api/pandas.json_normalize.html
     return  pd.json_normalize(json_data)
 

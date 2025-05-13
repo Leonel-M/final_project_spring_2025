30bf0e1546416adf5f6de74d2c5400568b852ab3
Leonel-M <leonel.amarquez92@gmail.com>
2025-05-13 08:50:05 -0500

    refactor: Creation of the Product class for more specific data functions.

--- data.py
+++ data.py
@@ -7,6 +7,12 @@ class DataFrame:
         self.path = in_path
         self.df = self.get_data()
 
+    def total(self):
+        """
+        :return:  Total amount of data per column 'id'
+        """
+        return len(self.df['id'].count())
+
     def get_data(self):
         """
         :return: Pandas DataFrames
@@ -23,14 +29,51 @@ class DataFrame:
             # https://pandas.pydata.org/docs/reference/api/pandas.json_normalize.html
         return  pd.json_normalize(json_data)
 
-    def info(self):
-        print(f'')
+    def get_info(self):
+        """
+        :return: DataFrame information
+        """
+        print(f'{self.df.info()}')
+        return
+
+class Product(DataFrame):
+    def __init__(self,in_path):
+        super().__init__(in_path)
+
+    def average(self):
+        """
+        :return: Average price of products.
+        """
+        return self.df['price'].mean()
+
+    def costlier(self):
+        """
+        :return:  the most expensive product (title and price)
+        """
+        # https://pandas.pydata.org/docs/reference/api/pandas.Series.idxmax.html
+
+        id_max = self.df['price'].idxmax()
+        product = self.df.loc[id_max,'title']
+        price = self.df.loc[id_max,'price']
+        print(f'{product} is the most expensive product, cost: {price}')
+        return product, price
+
+    def cheaper(self):
+        """
+        :return: the most cheap product (title and price)
+        """
+        id_min = self.df['price'].idxmin()
+        product = self.df.loc[id_min, 'title']
+        price = self.df.loc[id_min, 'price']
+        print(f'{product} is the most cheaper product, cost: {price}')
+        return product, price
+
 path_locations = 'data/locations.json'
 path_products = 'data/products.json'
 path_users = 'data/users.json'
 
 locations = DataFrame(path_locations)
-products = DataFrame(path_products)
+products = Product(path_products)
 users = DataFrame(path_users)
 
 

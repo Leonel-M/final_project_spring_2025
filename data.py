3400c17e59baba556af8568919208fdce32a6e98
Leonel-M <leonel.amarquez92@gmail.com>
2025-05-13 09:42:15 -0500

    refactor: Creation of the User class to handle user data

--- data.py
+++ data.py
@@ -55,7 +55,7 @@ class Product(DataFrame):
         id_max = self.df['price'].idxmax()
         product = self.df.loc[id_max,'title']
         price = self.df.loc[id_max,'price']
-        print(f'{product} is the most expensive product, cost: {price}')
+        # print(f'{product} is the most expensive product, cost: {price}')
         return product, price
 
     def cheaper(self):
@@ -65,8 +65,24 @@ class Product(DataFrame):
         id_min = self.df['price'].idxmin()
         product = self.df.loc[id_min, 'title']
         price = self.df.loc[id_min, 'price']
-        print(f'{product} is the most cheaper product, cost: {price}')
+        # print(f'{product} is the cheapest product, cost: {price}')
         return product, price
+
+class User(DataFrame):
+    def __init__(self,in_path):
+        super().__init__(in_path)
+        self.df['creationAt'] = self.date_format('creationAt')
+        self.df['updatedAt'] = self.date_format('updatedAt')
+
+    def date_format(self, column):
+        """
+        The format of the columns 'creationAt' and 'updateAt' are in ISO 8601 format.
+        YYYYY-MM-DDThh:mm:ss.fff.
+        it is necessary to transform it to dates that handle pandas with to_datetime.
+        :return: Date readable in pandas
+        """
+        #https://stackoverflow.com/questions/18618288/how-do-i-convert-dates-into-iso-8601-datetime-format-in-a-pandas-dataframe:
+        return  pd.to_datetime(self.df[column], utc=True)
 
 path_locations = 'data/locations.json'
 path_products = 'data/products.json'
@@ -74,6 +90,6 @@ path_users = 'data/users.json'
 
 locations = DataFrame(path_locations)
 products = Product(path_products)
-users = DataFrame(path_users)
+users = User(path_users)
 
 
